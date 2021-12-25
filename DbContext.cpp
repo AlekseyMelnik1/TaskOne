513a4adc8965cebf4772836a7ce87f1831bf0d9d
Aleksey Melnik <melnik@impulse-kiev.in.ua>
2021-12-25 23:32:29 +0200

    TaskOne

---
+++ DbContext.cpp
@@ -0,0 +1,113 @@
+#include "DbContext.h"
+
+
+DbContext::DbContext(QObject *parent) : QObject(parent)
+{
+
+}
+
+DbContext::~DbContext()
+{
+
+}
+
+
+void DbContext::connectToDataBase()
+{
+    if(!QFile("./" DATABASE_NAME).exists()){
+        this->restoreDataBase();
+    } else {
+        this->openDataBase();
+    }
+}
+
+
+bool DbContext::restoreDataBase()
+{
+
+    if(this->openDataBase()){
+
+        return (this->createTable()) ? true : false;
+    } else {
+        qDebug() << "Не удалось восстановить базу данных";
+        return false;
+    }
+    return false;
+}
+
+bool DbContext::openDataBase()
+{
+
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    //db.setHostName(DATABASE_HOSTNAME);
+    db.setDatabaseName("./" DATABASE_NAME);
+    if(db.open()){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void DbContext::closeDataBase()
+{
+    db.close();
+}
+
+
+
+bool DbContext::createTable()
+{
+
+    QSqlQuery query;
+
+    if(!query.exec( "CREATE TABLE " TABLE " ("
+                            TABLE_ONE      " VARCHAR(255)    NOT NULL,"
+                            TABLE_TWO      " VARCHAR(255)    NOT NULL,"
+                            TABLE_THREE    " VARCHAR(255)    NOT NULL,"
+                            TABLE_FOUR     " VARCHAR(255)    NOT NULL,"
+                            TABLE_FIVE     " VARCHAR(255)    NOT NULL,"
+                            TABLE_SIX      " VARCHAR(255)    NOT NULL"
+                        " )"
+                    )){
+        qDebug() << "DataBase: error of create " << TABLE;
+        return false;
+    }else{
+        return true;
+    }
+    return false;
+
+}
+
+void DbContext::insertIntoTable(const QList<QStringList> &data)
+{
+
+    QSqlQuery query;
+
+    qDebug()<<"Inserting";
+    int i=0;
+    for (auto &subdata : data){
+    query.prepare("INSERT INTO " TABLE " ( " TABLE_ONE ", "
+                                             TABLE_TWO ", "
+                                             TABLE_THREE ", "
+                                             TABLE_FOUR ", "
+                                             TABLE_FIVE ", "
+                                             TABLE_SIX " ) "
+                  "VALUES (:One, :Two, :Three, :Four, :Five, :Six )");
+    query.bindValue(":One",        data.at(i).at(0));
+    query.bindValue(":Two",        data.at(i).at(1));
+    query.bindValue(":Three",      data.at(i).at(2));
+    query.bindValue(":Four",       data.at(i).at(3));
+    query.bindValue(":Five",       data.at(i).at(4));
+    query.bindValue(":Six",        data.at(i).at(5));
+    // После чего выполняется запросом методом exec()
+    if(!query.exec()){
+        qDebug() << "error insert into " << TABLE;
+}
+     i++;
+
+}
+}
+
+
+
+
---
+++ main.cpp
@@ -0,0 +1,122 @@
+#include "DbContext.h"
+#include <QCoreApplication>
+#include <QList>
+#include <QString>
+#include <QStringList>
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
+#include <QFileInfo>
+
+
+QString GetRandomString()
+{
+   const QString possibleCharacters("Zabcdefghijklmnopqrstuvwxyz0123456789");
+   const int randomStringLength = 8; // assuming you want random strings of 8 characters
+
+   QString randomString;
+   for(int i=0; i<randomStringLength; ++i)
+   {
+       int index = qrand() % possibleCharacters.length();
+       QChar nextChar = possibleCharacters.at(index);
+       randomString.append(nextChar);
+   }
+   return randomString;
+}
+
+ QList<QStringList>  GetListString()
+{
+   QList<QStringList> test;
+    for(int i=0;i<SizeColumn;++i){
+        QList<QString> tmp;
+      for(int j=0;j<SizeOfRow;++j){
+      tmp.push_back(GetRandomString());
+      }
+       test.push_back(tmp);
+    }
+
+
+    return test;
+}
+
+
+void WriteToCSV(const QList<QStringList>& data,QString fileName)
+{
+    // Open csv-file
+    QFile file(fileName);
+    file.open(QIODevice::WriteOnly | QIODevice::Text);
+
+    // Write data to file
+    QTextStream stream(&file);
+    QString separator(",");
+    for (int i = 0; i < data.size(); ++i)
+    {
+        stream << data.at(i).join(separator) << "\n";
+    }
+
+    stream.flush();
+    file.close();
+}
+
+QList<QStringList> ReadCSV(QString fileName)
+{
+    // Open csv-file
+    QFile file(fileName);
+    file.open(QIODevice::ReadOnly | QIODevice::Text);
+
+    // Read data from file
+    QTextStream stream(&file);
+    QList<QStringList> data;
+    QString separator(",");
+    while (stream.atEnd() == false)
+    {
+        QString line = stream.readLine();
+        data << line.split(separator);
+    }
+
+    file.close();
+    return data;
+}
+
+void Print(const QList<QStringList>& data)
+{
+    for (int i = 0; i < data.size(); ++i)
+    {
+        qDebug() << data.at(i).join(", ");
+    }
+}
+QList<QStringList> ReplaceNumbersAndDeleteStringLargeLetters(QList<QStringList> &data){
+  int i=0;
+  for (auto &subdata : data){
+
+          i++;
+          for (auto &elstring : subdata){
+
+              elstring.replace(QRegExp("[13579]"), "#");
+              if(elstring[0].isUpper()){
+                    data.removeAt(i);
+              }
+
+          }
+
+}
+
+    return data;
+}
+
+int main(void)
+{
+
+    QList<QStringList> data=GetListString();
+    WriteToCSV(data,"First_out.csv");
+    QList<QStringList> dataReadCsv=ReadCSV("First_out.csv");
+    QList<QStringList> newDataAfterFunck=ReplaceNumbersAndDeleteStringLargeLetters(dataReadCsv);
+    WriteToCSV(newDataAfterFunck,"Second_out.csv");
+
+    DbContext myDb;
+    myDb.connectToDataBase();
+    myDb.insertIntoTable(ReadCSV("First_out.csv"));
+
+
+    return 0;
+}
